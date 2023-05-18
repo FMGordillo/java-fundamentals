9fda28b809dcfb5c58b8d4a47e8facededc47536
Facundo Martin Gordillo <me@facundogordillo.com>
2023-05-18 16:00:00 +0100

    feat: sup gross pay

--- src/main/java/basics/GrossPayCalculator.java
+++ src/main/java/basics/GrossPayCalculator.java
@@ -1,7 +1,17 @@
 package src.main.java.basics;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("How much did you work in a week?");
+		Scanner sc = new Scanner(System.in);
+		int hours = sc.nextInt();
+
+		System.out.println("What is your hourly pay rate?");
+		double rate = sc.nextDouble();
+
+		double grossPay = hours * rate;
+		System.out.println("Your gross pay is $" + grossPay);
 	}
 }
